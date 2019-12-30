2ea68882be769c30f4716e92cfe0c83280c0d54e
rajpatel <rajpateln1995@gmail.com>
2019-12-30 13:05:06 +0530

    dkk

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blogurls')),
+    path('', views.post_list, name='post_list'),
 ]
